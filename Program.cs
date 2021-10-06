cebf618a09af8fcd055018849012855db5cd839d
sagar <7208sk@gmail.com>
2021-10-07 00:13:43 +0530

    Creating line computation project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Line_Comparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line comparison computation");
+            Console.ReadLine();
+        }
+    }
+}
